09d3ea0223b81bab1d7d29b14eb50654011a159f
llkui <liukui178@163.com>
2017-02-23 15:56:27 +0800

    louhua

---
+++ production/js/main.js
@@ -0,0 +1,7 @@
+var apiUrl = 'http://192.168.1.3/xinlouhua';
+
+function GetQueryString(name){
+     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+     var r = window.location.search.substr(1).match(reg);
+     if(r!=null)return  unescape(r[2]); return null;
+}
