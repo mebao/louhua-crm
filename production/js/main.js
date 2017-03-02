ed7a721001533c7b4f687ff38b9cd3a0461b8813
llkui <liukui178@163.com>
2017-03-02 15:36:08 +0800

    louhua

--- production/js/main.js
+++ production/js/main.js
@@ -5,3 +5,15 @@ function GetQueryString(name){
      var r = window.location.search.substr(1).match(reg);
      if(r!=null)return  unescape(r[2]); return null;
 }
+
+var signIn = true;
+var username = localStorage.getItem('username');
+var token = localStorage.getItem('token');
+var role = localStorage.getItem('role');
+if(!(username && token && role)){
+	$('#loginIntercept').modal({backdrop: 'static', keyboard: false});
+	$('#login').click(function(){
+		location.href = 'louhua_login.html';
+	});
+	signIn = false;
+}
