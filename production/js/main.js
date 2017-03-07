9205efb879808626f62a5e4f833a25bd38341aa0
llkui <liukui178@163.com>
2017-03-07 15:45:41 +0800

    louhua

--- production/js/main.js
+++ production/js/main.js
@@ -17,3 +17,77 @@ if(!(username && token && role)){
 	});
 	signIn = false;
 }
+
+function sortPositiveByKey(array, key, type) {
+	return array.sort(function(a, b) {
+		if(type == 'num'){
+ 			var x = parseFloat(a[key]); var y = parseFloat(b[key]);
+		}else{
+ 			var x = a[key]; var y = b[key];
+		}
+ 		return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+	});
+}
+
+function sortNegativeByKey(array, key, type) {
+	return array.sort(function(a, b) {
+		if(type == 'num'){
+ 			var x = parseFloat(a[key]); var y = parseFloat(b[key]);
+		}else{
+ 			var x = a[key]; var y = b[key];
+		}
+ 		return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+	});
+}
+
+//array copy
+function arrayCopy(array){
+	var newArray = new Array();
+	for(var i = 0; i<array.length; i++){
+		newArray.push(array[i]);
+	}
+	return newArray;
+}
+
+
+//left sider共用
+$('.menu_section').html(
+'<ul class="nav side-menu">\
+    <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>\
+        <ul class="nav child_menu">\
+            <li><a href="louhua_home.html">home</a></li>\
+            <li><a href="louhua_adminlist.html">adminlist</a></li>\
+            <li><a href="louhua_agentlist.html">agentlist</a></li>\
+        </ul>\
+    </li>\
+    <li><a><i class="fa fa-edit"></i> Project Manage <span class="fa fa-chevron-down"></span></a>\
+        <ul class="nav child_menu">\
+            <li>\
+                <a href="louhua_projectList.html">project list</a>\
+            </li>\
+        </ul>\
+    </li>\
+    <li><a><i class="fa fa-edit"></i> House Manage <span class="fa fa-chevron-down"></span></a>\
+        <ul class="nav child_menu">\
+            <li>\
+                <a href="louhua_ihave.html">I have</a>\
+            </li>\
+            <li>\
+                <a href="louhua_iwant.html">I want</a>\
+            </li>\
+            <li>\
+                <a href="louhua_newHouse.html">Add House</a>\
+            </li>\
+        </ul>\
+    </li>\
+    <li><a><i class="fa fa-edit"></i> Task <span class="fa fa-chevron-down"></span></a>\
+        <ul class="nav child_menu">\
+            <li>\
+                <a href="louhua_tasklist.html">Task List</a>\
+            </li>\
+            <li>\
+                <a href="louhua_myTask.html">My Task</a>\
+            </li>\
+        </ul>\
+    </li>\
+</ul>');
